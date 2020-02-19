d366bc28f2c9bd35192476048b9d31ee998b0910
gaojun <qduprog@qq.com>
2020-02-19 21:02:04 +0800

    新增个人信息编辑页面

--- ranDian_dxf_smallRoutine/dxcs/pages/account/account.js
+++ ranDian_dxf_smallRoutine/dxcs/pages/account/account.js
@@ -5,43 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // 服务热线的样式
-    display: "fixed",
-    height: "188rpx"
+    realName:'高俊'
   },
   onLoad(){
-    var that = this;
-    var screenHeight, heights
-    wx.getSystemInfo({
-      success: function (res) {
-        screenHeight = res.screenHeight
-        // console.log(res.screenHeight)
-      }
-    });
-    //创建节点选择器
-    var query = wx.createSelectorQuery();
-    query.select('.main').boundingClientRect()
-    query.exec(function (res) {
-      //res就是 所有标签为xxxx的元素的信息 的数组
-      // console.log(res);
-      //取高度
-      heights = res[0].height;
-      if (screenHeight - heights <= 124) {
-        that.setData({
-          display: '',
-          height: "188rpx"
-        })
-      } else {
-        that.setData({
-          display: 'fixed',
-          height: "88rpx"
-        })
-      }
-    })
+    //加载用户账户数据
+
   },
   gotoRealName(){
     wx.navigateTo({
-      url:'../account_name/account_name'
+      url:'../account_name/account_name?realName=高俊'
+    })
+  },
+  gotoIdCard(){
+    wx.navigateTo({
+      url:'../account_idCard/account_idCard?idCard=1234567'
+    })
+  },
+  gotoPhone(){
+    wx.navigateTo({
+      url:'../account_phone/account_phone?phone=17621378282'
+    })
+  },
+  gotoSignature(){
+    wx.navigateTo({
+      url:'../account_signature/account_signature?signature=高俊'
     })
   }
 })
