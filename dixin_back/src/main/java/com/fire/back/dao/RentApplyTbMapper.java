3be0195ab308cb85b081c31c50c82daea5653d09
sunhengyuan <hy_pro@163.com>
2020-03-02 22:33:05 +0800

    [房租模块]---实体类mapper

--- dixin_back/src/main/java/com/fire/back/dao/RentApplyTbMapper.java
+++ dixin_back/src/main/java/com/fire/back/dao/RentApplyTbMapper.java
@@ -2,32 +2,21 @@ package com.fire.back.dao;
 
 import com.fire.back.entity.RentApplyTb;
 import com.fire.back.entity.RentApplyTbExample;
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RentApplyTbMapper {
-    int countByExample(RentApplyTbExample example);
-
-    int deleteByExample(RentApplyTbExample example);
-
     int deleteByPrimaryKey(Long id);
 
-    int insert(RentApplyTb record) throws DataAccessException;
+    int insert(RentApplyTb record);
 
     int insertSelective(RentApplyTb record);
 
     List<RentApplyTb> selectByExample(RentApplyTbExample example);
 
     RentApplyTb selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") RentApplyTb record, @Param("example") RentApplyTbExample example);
-
-    int updateByExample(@Param("record") RentApplyTb record, @Param("example") RentApplyTbExample example);
 
     int updateByPrimaryKeySelective(RentApplyTb record);
 
