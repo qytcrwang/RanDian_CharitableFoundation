61101213dfb775d7447c3ca3ad5c579b004a1ff6
Jackwang <wt373747285@163.com>
2020-03-03 12:22:29 +0800

    活动详情点赞切换

--- ranDian_dxf_smallRoutine/dxcs/pages/activity_detail/activity_detail.js
+++ ranDian_dxf_smallRoutine/dxcs/pages/activity_detail/activity_detail.js
@@ -8,25 +8,65 @@ Page({
     //活动详情
     activityDetails:null,
     //图片
-    imgs:[]
+    imgs:[],
+    currentPostId: ''
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    let activityId = options.id;
-    this.getActivityDetails(activityId);
+  changeGoodStatus(activityId){
+    // 设置活动id为页面共享
+    this.setData({
+      currentPostId: activityId
+    });
+    // 获取缓存
+    var goodList = wx.getStorageSync('goodList');
+    if (goodList) {
+      // 获取活动收藏状态
+      var goodStatus = goodList[activityId];
+      // 更新活动收藏状态
+      this.setData({
+        goodStatus: goodStatus
+      });
+    } else {
+      // 如果不存在缓存就设置一个新缓存
+      var goodList = {};
+      wx.setStorageSync('goodList', goodList);
+    }
+  },
+
+
+  ongoodTap: function (event) {
+    var goodList = wx.getStorageSync('goodList');
+    // 获取活动收藏状态
+    var goodStatus = goodList[this.data.currentPostId];
+    // 活动收藏状态切换
+    goodStatus = !goodStatus;
+    // 存储活动收藏状态
+    goodList[this.data.currentPostId] = goodStatus;
+    // 三个参数为,Storage键、值、活动收藏状态
+    this.showToast('goodList', goodList, goodStatus);
+  },
+
+  showToast: function (key, value, status) {
+
+    wx.setStorageSync(key, value);
+    // 更新活动收藏状态
+    this.setData({
+      goodStatus: status
+    });
+    // 显示提示
+    wx.showToast({
+      title: status ? "点赞成功" : "取消成功"
+    })
   },
 
   getActivityDetails(activityId){
     let that = this;
     wx.request({
       method:'POST',
-      data: {
-        id: activityId
-      },
+      // data: {
+      //   id: activityId
+      // },
       url:"http://mock-api.com/VKyv1Gzw.mock/activity/getInfo",
       success(res){
         if(res.data.status===1){
@@ -41,6 +81,17 @@ Page({
       }
     })
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let activityId = options.id;
+    this.getActivityDetails(activityId);
+    this.changeGoodStatus(activityId);
+  },
+
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
