bfbd185fd13bf2071ef21cd77889c12fcf34edee
georgegao2019 <qduprog@qq.com>
2020-03-04 20:34:51 +0800

    首页更新

--- ranDian_dxf_smallRoutine/dxcs/pages/index/index.js
+++ ranDian_dxf_smallRoutine/dxcs/pages/index/index.js
@@ -9,14 +9,28 @@ Page({
     // service_tel: "0551-12345678", 服务电话
     // complaint_tel: "0551-12345678" 投诉电话
     setting: [],
-    //推荐视频
-    othersList:[],
     //轮播图数据
-    swiperList:[]
+    bannerList:[
+      {imgSrc:'/img/activity1.png'},
+      {imgSrc:'/img/activity2.png'},
+      {imgSrc:'/img/activity3.png'}
+    ],
+    //推荐活动列表
+    recommendActivityList:[
+      {id:'1',imgSrc:'/img/activity1.png',title:'老年中心举办了此次活动'},
+      {id:'2',imgSrc:'/img/activity2.png',title:'老年中心举办了此次活动'},
+      {id:'3',imgSrc:'/img/activity3.png',title:'老年中心举办了此次活动'},
+      {id:'4',imgSrc:'/img/activity1.png',title:'老年中心举办了此次活动'}
+    ],
+    list:[
+      {id:'',imgSrc:'/img/activity1.png',title:'标题',content:'老年中心举办了此次活动'},
+      {id:'',imgSrc:'/img/activity2.png',title:'标题',content:'老年中心举办了此次活动'},
+      {id:'',imgSrc:'/img/activity3.png',title:'标题',content:'老年中心举办了此次活动'}
+    ]
   },
 
   //获取首页轮播图
-  getSwiperList(){
+  /*getSwiperList(){
     let that = this;
     wx.request({
       url:"http://mock-api.com/VKyv1Gzw.mock/swiperList",
@@ -28,24 +42,7 @@ Page({
         }
       }
     })
-  },
-
-  /**
-   * 视频推荐列表
-   */
-  getOthersList(){
-    let that = this;
-    wx.request({
-      url:"http://mock-api.com/VKyv1Gzw.mock/othersList",
-      success(res){
-        if(res.data.code===0){
-          that.setData({
-            othersList:res.data.data.othersList
-          })
-        }
-      }
-    })
-  },
+  },*/
 
   onShow: function () {
     wxb.that = this;   //正确打开海天应用的方式
@@ -57,8 +54,8 @@ Page({
     wxb.that = this;
     wxb.style();
     this.getHome();
-    this.getSwiperList();
-    this.getOthersList();
+    //this.getSwiperList();
+    //this.getOthersList();
   },
 
   getHome: function (e) {
