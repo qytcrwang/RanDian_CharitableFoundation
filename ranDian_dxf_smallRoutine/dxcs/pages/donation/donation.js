10b3ce1f2090ed620b00064ab9ac0ea3d02c29af
georgegao2019 <qduprog@qq.com>
2020-03-11 19:41:35 +0800

    捐赠列表提交接口

--- ranDian_dxf_smallRoutine/dxcs/pages/donation/donation.js
+++ ranDian_dxf_smallRoutine/dxcs/pages/donation/donation.js
@@ -1,13 +1,23 @@
+var wxb = require('../../utils/wxb.js');
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     donateMoneyTab:true,
-    donateGoodsTab:false
+    donateGoodsTab:false,
+    userid:'',
   },
   onLoad: function () {
+    var _this = this;
+    wx.getStorage({
+      key:'userid',
+      success:function(res){
+        _this.setData({
+          userid:res.data
+        })
+      }
+    })
   },
 
   toDonateMoney: function (e) {
@@ -22,4 +32,32 @@ Page({
       donateGoodsTab: true
     })
   },
+  //提交捐赠信息
+  doncationSubmit:function(e){
+    var contriType = this.data.donateMoneyTab? "1":"0";
+    var contriAmount = this.data.donateMoneyTab? e.detail.value.contriAmount:"";
+    var contriThings = this.data.donateMoneyTab? "":e.detail.value.contriThings;
+    wxb.wxPost(
+      '/contriInfo/saveContriInfo',
+      {
+        userId:this.data.userid,
+        contriType:contriType,
+        contriAmount:contriAmount,
+        contriThings:contriThings,
+      },function(backResult){
+        if(backResult.status == 1){
+          wx.showToast({
+            title:backResult.msg,
+            icon:'success',
+            duration:2000
+          })
+        }else{
+          wx.showToast({
+            title:backResult.msg,
+            duration:2000
+          })
+        }
+      }
+    )
+  }
 })
--- ranDian_dxf_smallRoutine/dxcs/pages/notify/notify.js
+++ ranDian_dxf_smallRoutine/dxcs/pages/notify/notify.js
@@ -1,42 +1,68 @@
 
 // 获取全局应用程序实例对象
 const app = getApp();
-
+var wxb = require("../../utils/wxb");
 // 创建页面实例对象
 Page({
-  /**
-   * 页面名称
-   */
-  name: "notify",
-  /**
-   * 页面的初始数据
-   */
-
   data: {
-    
-  
-  
-  },
+    trendsTap:true,
+    newsTap:false,
 
+    //动态列表数组
+    trendsList:[
+      {id:'',title:'',time:'',state:'',ifRead:''}
+    ],
+    //消息列表数组
+    newsList:[{
+      id:'1',title:'恭喜您注册成功',time:'2020-03-09',state:'',ifRead:''}
+    ],
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad () {
-    // 注册coolsite360交互模块
-    coolsite360.register(this);
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady () {
-
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow () {
+    var _this = this;
+    wx.getStorage({
+      key:'userid',
+      success:function(res){
+        wxb.wxPost(
+          '/notice/getNoticesWithOutContext',
+          {
+            msgType:'0'
+          },function(backResult){
+            console.log(backResult)
+            if(backResult.status == 1){
+              _this.setData({
+                trendsList:backResult.data
+              })
+            }else{
+              wx.showToast({
+                image:'/img/close.png',
+                title:'请求超时',
+                duration:2000
+              })
+            }
+          }
+        )
+      },
+      fail:function(res){
+        wx.showToast({
+          image:'/img/close.png',
+          title:'未获取到用户数据',
+          duration:2000
+        })
+      }
+    })
   },
 
   /**
@@ -62,6 +88,19 @@ Page({
 
 
   //以下为自定义点击事件
-  
+  //切换到动态列表页
+  togoTrendsTap(){
+    this.setData({
+      trendsTap:true,
+      newsTap:false,
+    })
+  },
+  //切换到消息列表页
+  togoNewsTap(){
+    this.setData({
+      trendsTap:false,
+      newsTap:true,
+    })
+  }
 })
 
