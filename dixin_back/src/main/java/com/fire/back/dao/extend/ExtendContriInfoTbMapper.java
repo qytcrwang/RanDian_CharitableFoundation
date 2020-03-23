fd1acbbdf26d9da36c1d74e33c2b17e9c2fef5d6
王凯 <kaiser156@126.com>
2020-03-23 08:59:11 +0800

    juan

--- dixin_back/src/main/java/com/fire/back/dao/extend/ExtendContriInfoTbMapper.java
+++ dixin_back/src/main/java/com/fire/back/dao/extend/ExtendContriInfoTbMapper.java
@@ -1,6 +1,5 @@
 package com.fire.back.dao.extend;
 
-import com.fire.back.dao.ContriInfoTbMapper;
 import com.fire.back.dto.ContriInfoListParamsDto;
 import com.fire.back.entity.ContriInfoTb;
 import java.util.List;
