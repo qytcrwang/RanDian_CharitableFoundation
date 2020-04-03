624d803ef69b76521593a1258694548cb8a886a5
HuaQuanChiDaShe <38030389+HuaQuanChiDaShe@users.noreply.github.com>
2020-04-03 10:32:53 +0800

    登录跳转

--- dixin_back/src/main/java/com/fire/back/config/ShiroConfig.java
+++ dixin_back/src/main/java/com/fire/back/config/ShiroConfig.java
@@ -66,7 +66,7 @@ public class ShiroConfig {
 		// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
 		shiroFilterFactoryBean.setLoginUrl("/login");
 		// 登录成功后要跳转的链接
-		shiroFilterFactoryBean.setSuccessUrl("/index");
+		shiroFilterFactoryBean.setSuccessUrl("/");
 
 		//未授权界面;
 		shiroFilterFactoryBean.setUnauthorizedUrl("/403");
